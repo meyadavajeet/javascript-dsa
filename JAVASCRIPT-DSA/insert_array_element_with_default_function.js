c520cbaa62baa02ea51e0f323c12c2d8d08b57ee
meyadavajeet <meyadavajeet@gmail.com>
2022-07-24 14:21:43 +0530

    insert array element using default function of javascript

---
+++ JAVASCRIPT-DSA/insert_array_element_with_default_function.js
@@ -0,0 +1,8 @@
+// insert an element on array using in built function of javascript
+//splice(20,0,509);
+// splice(position,want to delete,newElement)
+
+let arr = [30,40,0505,05,590];
+arr.splice(4,0,509);
+console.log(arr);
+
