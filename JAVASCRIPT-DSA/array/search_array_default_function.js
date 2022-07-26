624392accb21a882cc2179bc6917be70b0f424b5
meyadavajeet <meyadavajeet@gmail.com>
2022-07-27 01:18:36 +0530

    serching in an array

---
+++ JAVASCRIPT-DSA/array/search_array_default_function.js
@@ -0,0 +1,6 @@
+let array = [1, 2, 3, 4, 5, 8, 9, 6, 90, 70, 567, 2345, 85, 97];
+// let itemToSearch = 97;
+let itemToSearch = 90;
+let searchResult = array.indexOf(itemToSearch);
+
+console.log(searchResult,' search item index');
