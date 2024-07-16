fc97ab8ae8ea56969bc4d2b878e2ba840e4b3a2e
meyadavajeet <meyadavajeet@gmail.com>
2024-07-17 03:37:35 +0530

    renove extra zeros

--- JAVASCRIPT-DSA/array/reverse_array_using_default_function.js
+++ JAVASCRIPT-DSA/array/reverse_array_using_default_function.js
@@ -1,4 +1,4 @@
-let arr = [1,40,0505,05,590, 8, 9, 6, 90, 70, 567];
+let arr = [1, 40, 505, 5, 590, 8, 9, 6, 90, 70, 567];
 let reverseArray = arr.reverse();
-console.log('reversed Array: ' + reverseArray);
+console.log("reversed Array: " + reverseArray);
 // console.log(reverseArray);
