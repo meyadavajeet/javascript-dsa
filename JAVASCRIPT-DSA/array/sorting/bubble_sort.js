bd48a8c5ce40da1ed17288647709d850e3762e9d
meyadavajeet <meyadavajeet@gmail.com>
2024-07-19 23:20:18 +0530

    array sort

---
+++ JAVASCRIPT-DSA/array/sorting/bubble_sort.js
@@ -0,0 +1,19 @@
+function sortAnArray() {
+  let arr = [13, 14, 12, 11, 23];
+  let len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        console.log(arr);
+      }
+    }
+  }
+  console.log("final sorted array");
+  console.log(arr);
+}
+
+sortAnArray();
