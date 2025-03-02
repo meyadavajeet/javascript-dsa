81f55cceb8cb2fda3cf8300b91ccbfcc33b9c8f7
meyadavajeet <meyadavajeet@gmail.com>
2025-03-03 01:44:26 +0530

    array bubble sort

--- practice/nestedforloop.js
+++ practice/nestedforloop.js
@@ -25,7 +25,7 @@ const numberDivisibleBy5 = () => {
         if (startingNumber % 5 === 0) {
             console.log(startingNumber);
         }
-        startingNumber ++
+        startingNumber++
     }
 }
 
@@ -34,15 +34,32 @@ const numberDivisibleBy5 = () => {
 
 // Q- Use for-in loop to iterate over an object and log its keys
 const forInLoopExample = (obj) => {
-    for(let ob in obj){
+    for (let ob in obj) {
         console.log(ob)
     }
 }
 
 const obj = {
-    name : "Ajeet",
-    age : 26,
-    empId : 47
+    name: "Ajeet", age: 26, empId: 47
 }
 
-forInLoopExample(obj);
+// forInLoopExample(obj);
+
+
+// write a program that will sort the array in ascending order using bubble sort
+const arraySorting = (arr) => {
+    // console.log(arr)
+    for (let j = 0; j < (arr.length - 1); j++) {
+        for (let i = 0; i < (arr.length - 1 - j); i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+
+        }
+    }
+    console.log(arr)
+}
+const arr = [8, 2, 1, 849, 990, 99, 90];
+arraySorting(arr);
