2dc8af315dbe028b7be4b9276c1d2e04584d9759
meyadavajeet <meyadavajeet@gmail.com>
2025-03-05 09:20:22 +0530

    fixed code of sum of elements to zero

--- practice/sum_of_array_to_element_to_zero.js
+++ practice/sum_of_array_to_element_to_zero.js
@@ -1,19 +1,24 @@
-
 const findSumOfArrayElement = (arr) => {
-  leftIndex = arr[0];
-  rightIndex = arr.length;
-  while (leftIndex < rightIndex) {
-    let sum = leftIndex + rightIndex;
-    if(sum === 0){
-      return console.log([leftIndex,rightIndex]);
-    }elseif(result > 0){
-      rightIndex --;
-    }elseif(result < 0){
-      leftIndex ++;
-    }else{
-      console.log("Something went wrong")
+    let leftIndex = arr[0];
+    let rightIndex = arr.length;
+    while (leftIndex < rightIndex) {
+        let sum = leftIndex + rightIndex;
+        if (sum === 0) {
+            return console.log([leftIndex, rightIndex]);
+        }
+        else if(sum > 0)
+        {
+            rightIndex--;
+        }
+        else if(sum < 0)
+        {
+            leftIndex++;
+        }
+    else
+        {
+            console.log("Something went wrong")
+        }
     }
-  }
 }
 
 let arr = [-4, -3, 0, 1, 3, 4, 6, 8];
