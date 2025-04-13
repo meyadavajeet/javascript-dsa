75410f6351386e1765c13f8f3b48199eafbd5c24
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-04-13 11:34:57 +0530

    rotate element with k steps

--- JAVASCRIPT-DSA/array/problems/7.js
+++ JAVASCRIPT-DSA/array/problems/7.js
@@ -24,12 +24,55 @@ function shiftToLeft(arr) {
  */
 
 function shiftToRight(arr) {
-  let lastIndexValue = arr[arr.length-1];
-  for (let i = arr.length -1; i > 0  ; i--) {
-    arr[i] = arr[i-1]
+  let lastIndexValue = arr[arr.length - 1];
+  for (let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
   }
   arr[0] = lastIndexValue;
-  console.log(arr)
+  console.log(arr);
   return arr;
 }
-shiftToRight([1, 2, 3, 4, 5]);
+// shiftToRight([1, 2, 3, 4, 5]);
+
+/**
+ * ? left rotation by k step
+ * suppose k = 2 - it means rotate this to 2 step
+ * input - [1, 2, 3, 4, 5]
+ * output - [ 3, 4, 5, 1, 2]
+ */
+function leftRotationKStep(arr, k) {
+  k = k % arr.length;
+  for (j = 0; j < k; j++) {
+    console.log(j);
+    let startValue = arr[0];
+    let lastIndex = arr.length - 1;
+    for (let i = 0; i < lastIndex; i++) {
+      arr[i] = arr[i + 1];
+    }
+    arr[lastIndex] = startValue;
+  }
+  console.log(arr);
+  return arr;
+}
+
+// leftRotationKStep([1, 2, 3, 4, 5], 2);
+
+/**
+ * ? array right rotation by k steps
+ *
+ */
+
+function arrayRightRotationByKStep(arr, k) {
+  k = k % arr.length;
+  for (let j = 1; j <= k; j++) {
+    let lastIndexValue = arr[arr.length - 1];
+    for (let i = arr.length - 1; i > 0; i--) {
+      arr[i] = arr[i - 1];
+    }
+    arr[0] = lastIndexValue;
+  }
+  console.log(arr);
+  return arr;
+}
+
+arrayRightRotationByKStep([1, 2, 3, 4, 5], 2);
