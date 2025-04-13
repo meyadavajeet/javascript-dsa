9557c17f8c1799691a9a1a7712a423ac14bd72a7
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-04-13 10:52:05 +0530

    shift to right

--- JAVASCRIPT-DSA/array/problems/7.js
+++ JAVASCRIPT-DSA/array/problems/7.js
@@ -1,4 +1,5 @@
-// ? shift elements to left
+// ? shift elements to left or left rotation by 1
+
 /**
  * input [1,2,3,4,5]
  * output [2,3,4,5,1]
@@ -6,12 +7,29 @@
 function shiftToLeft(arr) {
   let startValue = arr[0];
   let lastIndex = arr.length - 1;
-  for(let i =0; i<=lastIndex; i++){
-    arr[i] = arr[i+1]
+  for (let i = 0; i <= lastIndex; i++) {
+    arr[i] = arr[i + 1];
   }
   arr[lastIndex] = startValue;
   console.log(arr);
   return arr;
 }
 
-shiftToLeft([1,2,3,4,5]);
+// shiftToLeft([1,2,3,4,5]);
+
+/**
+ * ? right rotation by 1
+ * input [1,2,3,4,5]
+ * output [5,1,2,3,4]
+ */
+
+function shiftToRight(arr) {
+  let lastIndexValue = arr[arr.length-1];
+  for (let i = arr.length -1; i > 0  ; i--) {
+    arr[i] = arr[i-1]
+  }
+  arr[0] = lastIndexValue;
+  console.log(arr)
+  return arr;
+}
+shiftToRight([1, 2, 3, 4, 5]);
