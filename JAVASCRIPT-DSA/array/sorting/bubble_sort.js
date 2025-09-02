23a29cf22fa0007602bbabf635a72650aaf8e42c
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-09-02 15:55:55 +0530

    sort

--- JAVASCRIPT-DSA/array/sorting/bubble_sort.js
+++ JAVASCRIPT-DSA/array/sorting/bubble_sort.js
@@ -17,3 +17,20 @@ function sortAnArray() {
 }
 
 sortAnArray();
+
+
+function sortAnArray1() {
+  let arr = [13, 14, 12, 11, 23];
+  let len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // ES6 swap
+      }
+    }
+  }
+  console.log("Sorted Array:", arr);
+}
+
+sortAnArray1();
