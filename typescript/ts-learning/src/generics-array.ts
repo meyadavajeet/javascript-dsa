43ea3ecc00b4645a3de7e458e914b2e00a81469b
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-09-11 12:20:06 +0530

    generic function and array

--- typescript/ts-learning/src/generics-array.ts
+++ typescript/ts-learning/src/generics-array.ts
@@ -7,3 +7,40 @@ const getMoreSearchProducts = <T>(products: T[]): T => {
   const myIndex = 22;
   return products[myIndex];
 };
+
+function anotherFunction<T, U>(valueOne: T, valueTwo: U): Object {
+  return {
+    valueOne,
+    valueTwo,
+  };
+}
+
+anotherFunction(3, "4");
+
+interface DatabaseInfo {
+  dbName: string;
+  username: string;
+  passwrod: string;
+  connectionString: string;
+  port: number;
+}
+
+function anotherFunction1<T, DatabaseInfo>(
+  valueOne: T,
+  dbInfo: DatabaseInfo
+): Object {
+  return {
+    valueOne,
+    dbInfo,
+  };
+}
+
+const dbInfo: DatabaseInfo = {
+  dbName: "something",
+  username: "something",
+  passwrod: "something",
+  connectionString: "something",
+  port: 123,
+};
+
+anotherFunction1(2, dbInfo);
